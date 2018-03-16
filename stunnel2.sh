dde6253dd55e4866ba0dd78a908304cda6e59ed6
claydersons <watashisousa@hotmail.com>
2018-03-16 03:34:43 -0300

    Update stunnel2.sh

--- stunnel2.sh
+++ stunnel2.sh
@@ -1,1 +1,1 @@
-wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel4 && wget http://freedomssh.com/vpsmanager/scripts/SSL/2/stunnel.conf && wget http://freedomssh.com/vpsmanager/scripts/SSL/2/stunnel.sh && sh stunnel.sh
+wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel4 && wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel.conf && wget http://freedomssh.com/vpsmanager/scripts/SSL/2/stunnel.sh && sh stunnel.sh
