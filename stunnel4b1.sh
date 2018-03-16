b51280a9182be632c48317ee736fa0a26b74aebe
claydersons <watashisousa@hotmail.com>
2018-03-16 20:19:47 -0300

    Update stunnel4b1.sh

--- stunnel4b1.sh
+++ stunnel4b1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget http://freedomssh.com/freedom/scripts/SSL/2/stunnel4f1
+wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel4f1
 rm /etc/default/stunnel4
 cat stunnel4f1 >> /etc/default/stunnel4
 rm stunnel4f1
