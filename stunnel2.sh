c759f2e83978add6212028c0ef20218ba3e3beb4
claydersons <watashisousa@hotmail.com>
2018-03-16 19:31:33 -0300

    Delete stunnel2.sh

--- stunnel2.sh
+++
@@ -1,5 +0,0 @@
-wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel4 
-wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel.conf 
-wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel.sh  
-chmod +x stunnel.sh 
-./stunnel.sh
