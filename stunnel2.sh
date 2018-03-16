d845303325f58c327bfc5186f4f89c9b97e96f28
claydersons <watashisousa@hotmail.com>
2018-03-16 05:44:17 -0300

    Update stunnel2.sh

--- stunnel2.sh
+++ stunnel2.sh
@@ -1,1 +1,5 @@
-wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel4 && wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel.conf && wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel.sh  && chmod +x stunnel.sh && stunnel.sh
+wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel4 
+wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel.conf 
+wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel.sh  
+chmod +x stunnel.sh 
+./stunnel.sh
